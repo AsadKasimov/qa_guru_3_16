c10f351a3cde92939e7e33ba7a4fbab6c0d4668d
GithubRedMouth <90645738+GithubRedMouth@users.noreply.github.com>
2023-02-13 00:35:22 +0300

    изменен под jenkins и selenoid

--- conftest.py
+++ conftest.py
@@ -1,12 +1,46 @@
+import os
 import pytest
 from selene.support.shared import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from dotenv import load_dotenv
+import attach
 
 
-@pytest.fixture(scope="session", autouse=True)
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
+
+
+@pytest.fixture(scope="function", autouse=True)
 def open_browser():
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
+    driver = webdriver.Remote(
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        options=options)
+    browser.config.driver = driver
+
     browser.config.window_width = 300
     browser.config.window_height = 700
-
+    browser.config.base_url = 'https://demoqa.com'
     yield
+
+    attach.add_html(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_video(browser)
 
     browser.quit()
